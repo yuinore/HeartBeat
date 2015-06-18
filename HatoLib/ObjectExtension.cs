7cb10e65e8249e239ae68fb6f89334218800e4d8
yuinore <yuinore@hotmail.co.jp>
2015-06-18 19:41:28 +0900

    Add ObjectExtension.cs

---
+++ HatoLib/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatoLib
+{
+    static class ObjectExtension
+    {
+        public static bool TryCast<T>(this object obj, out T result) where T : class
+        {
+            return (result = obj as T) != null;
+        }
+    }
+}
