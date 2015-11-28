c1b4a1edc9b6d0ef49966b02d06c28062eb72a9e
yuinore <yuinore@hotmail.co.jp>
2015-11-28 16:19:35 +0900

    SmartChartGeneratorを追加

---
+++ SmartChartGenerator/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SmartChartGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 52);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(742, 452);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.DragDrop += new System.Windows.Forms.DragEventHandler(this.chart1_DragDrop);
+            this.chart1.DragEnter += new System.Windows.Forms.DragEventHandler(this.chart1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(679, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(660, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 516);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.MinimumSize = new System.Drawing.Size(320, 240);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SmartChartGenerator/Form1.cs
@@ -0,0 +1,215 @@
+﻿using HatoBMSLib;
+using HatoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SmartChartGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_DragDrop(object sender, DragEventArgs e)
+        {
+            String[] filenames = ((string[])e.Data.GetData(DataFormats.FileDrop));
+
+            if (filenames.Length >= 1)
+            {
+                textBox1.Text = filenames[0];
+
+                Analyze();
+            }
+        }
+
+        private void chart1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void Analyze()
+        {
+            string filename = textBox1.Text;
+
+            if (!File.Exists(filename)) return;
+
+            BMSStruct b = new BMSStruct(filename);
+
+            //MessageBox.Show(b.PlayableBMObjects.Count + "notes");
+
+            // 集計
+            #region オブジェ数計算
+            Dictionary<int, int> SecondToNotesCount = new Dictionary<int, int>();
+            Dictionary<int, int> SecondToScratchCount = new Dictionary<int, int>();
+            int maxSec = 10;
+            int maxNotes = 5;
+            {
+                foreach (var obj in b.PlayableBMObjects)
+                {
+                    int sec = (int)Math.Floor(obj.Seconds);
+                    SecondToNotesCount[sec] = SecondToNotesCount.GetValueOrDefault(sec) + 1;
+                    SecondToScratchCount[sec] = SecondToScratchCount.GetValueOrDefault(sec) + (obj.Keyid == 6 ? 1 : 0);
+
+                    maxSec = Math.Max(sec, maxSec);
+                    maxNotes = Math.Max(maxNotes, SecondToNotesCount[sec]);
+                }
+            }
+            #endregion
+
+            #region オブジェ密度計算
+            Dictionary<int, double> RegionToMaxDensity = new Dictionary<int, double>();
+            Dictionary<int, double> RegionToMaxScratchDensity = new Dictionary<int, double>();
+            double regionWidthSeconds = 1;
+            double averagingWidthSeconds = 3.0;  // seconds
+
+            Func<IEnumerable<BMObject>, Dictionary<int, double>> GetDensityDictionary = (objs) =>
+            {
+                if (objs.Count() == 0) return new Dictionary<int, double>();
+
+                Dictionary<double, double> SecondToDensity = new Dictionary<double, double>();
+                {
+                    var nSecAve = objs.Select(x => Tuple.Create(x.Seconds, 1.0)).Concat(objs.Select(x => Tuple.Create(x.Seconds + averagingWidthSeconds, -1.0))).OrderBy(x => x.Item1);
+
+                    int duration = (int)Math.Floor(nSecAve.Last().Item1) + 1;
+
+                    nSecAve = nSecAve.Concat(Enumerable.Range(0, (int)((duration + averagingWidthSeconds + 10) / regionWidthSeconds)).Select(x => Tuple.Create((double)x * regionWidthSeconds, 0.0))).OrderBy(x => x.Item1);
+
+                    double maxObjCount = 0;
+                    double maxDensityAt = -1;
+                    double currentObjCount = 0;
+
+                    foreach (var o in nSecAve)
+                    {
+                        currentObjCount += o.Item2;
+                        if (currentObjCount > maxObjCount)
+                        {
+                            maxObjCount = currentObjCount;
+                            maxDensityAt = o.Item1;
+                        }
+
+                        SecondToDensity[o.Item1] = currentObjCount / averagingWidthSeconds;
+                    }
+
+                    Console.WriteLine("max density : " + (maxObjCount / averagingWidthSeconds) + " at " + maxDensityAt);
+                }
+
+                Dictionary<int, double> dict = new Dictionary<int, double>();
+
+                foreach (var kvpair in SecondToDensity)
+                {
+                    int secondInteger = (int)Math.Floor(kvpair.Key / regionWidthSeconds);
+                    dict[secondInteger] = Math.Max(kvpair.Value, dict.GetValueOrDefault(secondInteger));
+                }
+
+                return dict;
+            };
+
+            RegionToMaxDensity = GetDensityDictionary(b.PlayableBMObjects);
+            RegionToMaxScratchDensity = GetDensityDictionary(b.PlayableBMObjects.Where(x => x.Keyid == 6));
+
+
+            #endregion
+
+            //軸ラベルの設定
+            //chart1.ChartAreas[0].AxisX.Title = "angle(rad)";
+            //chart1.ChartAreas[0].AxisY.Title = "sin";
+            //※Axis.TitleFontでフォントも指定できるがこれはデザイナで変更したほうが楽
+
+            //X軸最小値、最大値、目盛間隔の設定
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = (int)(maxSec + 5);
+            chart1.ChartAreas[0].AxisX.Interval = 20;
+
+            //Y軸最小値、最大値、目盛間隔の設定
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = maxNotes / 5 * 5 + 10;
+            chart1.ChartAreas[0].AxisY.Interval = 5;
+            
+            // グラフのテスト
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+            
+            Series test4 = new Series();
+            {
+                test4.ChartType = SeriesChartType.StackedColumn;
+                test4.Color = Color.FromArgb(0xFF, 0x33, 0x66);
+                test4.Name = "Scratch Count";
+
+                for (int i = 0; i < maxSec + 2 + 5; i++)
+                {
+                    //if (SecondToScratchCount.GetValueOrDefault(i) != 0)
+                    //{
+                    test4.Points.AddXY(i, SecondToScratchCount.GetValueOrDefault(i));
+                    //}
+                }
+
+                chart1.Series.Add(test4);
+            }
+
+            Series test = new Series();
+            {
+                test.ChartType = SeriesChartType.StackedColumn;
+                test.Color = Color.FromArgb(0x99, 0xCC, 0x66);
+                test.Name = "Notes Count";
+
+                for (int i = 0; i < maxSec + 2 + 5; i++)
+                {
+                    test.Points.AddXY(i, SecondToNotesCount.GetValueOrDefault(i) - SecondToScratchCount.GetValueOrDefault(i));
+                }
+
+                chart1.Series.Add(test);
+            }
+
+            Series test2 = new Series();
+            {
+                test2.ChartType = SeriesChartType.Line;
+                test2.Color = Color.FromArgb(0x00, 0x66, 0xCC);
+                test2.BorderWidth = 2;
+                test2.Name = "Moving Ave (3sec)";
+
+                for (int i = 0; i < (maxSec + averagingWidthSeconds + 1) / regionWidthSeconds; i++)
+                {
+                    test2.Points.AddXY((double)i * regionWidthSeconds, RegionToMaxDensity.GetValueOrDefault(i) + 0.0001);
+                }
+
+                chart1.Series.Add(test2);
+            }
+
+            Legend legend = new Legend();
+            legend.DockedToChartArea = chart1.ChartAreas[0].Name;
+            legend.Alignment = StringAlignment.Near;
+            legend.Docking = Docking.Left | Docking.Top;
+            chart1.Legends.Add(legend);
+
+            /*Series test3 = new Series();
+            {
+                test3.ChartType = SeriesChartType.Line;
+                test3.Color = Color.FromArgb(0xFF, 0x00, 0x33);
+                test3.BorderWidth = 2;
+
+                for (int i = 0; i < (maxSec + averagingWidthSeconds + 1) / regionWidthSeconds; i++)
+                {
+                    test3.Points.AddXY((double)i * regionWidthSeconds, RegionToMaxScratchDensity.GetValueOrDefault(i) + 0.0001);
+                }
+
+                chart1.Series.Add(test3);
+
+                Legend legend = new Legend();
+                legend.DockedToChartArea = "Density (Scratch)";
+                legend.Alignment = StringAlignment.Near;
+                chart1.Legends.Add(legend);
+            }*/
+        }
+    }
+}
